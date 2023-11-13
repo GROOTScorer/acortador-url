c204e00c218f4be25dc755d0a92476869782a7c6
GROOTScorer <gaeldlk@gmail.com>
2023-11-13 11:26:49 -0300

    Cambios al sitio principal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import Navegador from './Navegador.js';
 import Main from './Main.js';
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from 'react';
+
+function Titulo() {
+  useEffect(() => {
+    document.title = 'Short-Link';
+  }, []);
+}
 
 function App() {
   return (
     <>
-          <Navegador />
+      <Navegador />
+      <Titulo />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/Navegador" element={<Navegador />} />
--- src/Main.js
+++ src/Main.js
@@ -1,28 +1,23 @@
-import { useEffect } from 'react';
 import './Main.css';
-
-function Titulo() {
-  useEffect(() => {
-    document.title = 'Short-Link';
-  }, []);
-}
 
 function Main() {
   return (
     <>
-    <div className="Container">
-      <Titulo />
-    <label htmlFor="link">Link:</label>
-    <input type="text" name="link" id="idlink" />
+    <div className='Home'>
+      <div className="Container">
+      <label htmlFor="link">Link:</label>
+      <input type="text" name="link" id="idlink" />
 
-    <br/>
-    
-    <button name="Boton1" id="IdBoton1">Recargar</button> <button name="Boton2" id="IdBoton2">Recortar</button>
-    <h3>Short-Link</h3>
+      <br/>
+      
+      <button name="Boton1" id="IdBoton1">Recargar</button> <button name="Boton2" id="IdBoton2">Recortar</button>
+      <h3>Short-Link</h3>
 
-    <label htmlFor="url">URL:</label>
-    <input type="text" name="URL" id="idurl" />
+      <label htmlFor="url">URL:</label>
+      <input type="text" name="URL" id="idurl" />
 
+      </div>
+      <footer>Creado por: Gael De Luca - Joaquín Pocovi - Mateo Villegas - Matías Zaracho</footer>
     </div>
 
     </>
