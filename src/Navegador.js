cfbb62d5796669f4f9bbfa5251ed19151d962467
GROOTScorer <gaeldlk@gmail.com>
2023-11-20 16:04:13 -0300

    Agrega Render Condicional según el estado de Login

--- src/Navegador.js
+++ src/Navegador.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import React from "react";
 import './Navegador.css';
 
-function Navegador() {
+function Navegador({ isLoggedIn, onLogout }) {
 
   return (
     <div className="Nav">
@@ -10,9 +10,15 @@ function Navegador() {
         <Link to='/'>Short-Link</Link>
         <ul className="navul">
           <li className="crear"><Link to='/Biblioteca'>Biblioteca</Link></li>
-          <li><Link to='/Registro'>Registrarse</Link></li>
-          <li><Link to='/Login'>Login</Link></li>
-
+          {!isLoggedIn && (
+            <>
+              <li><Link to='/Registro'>Registrarse</Link></li>
+              <li><Link to='/Login'>Login</Link></li>
+            </>
+          )}
+          {isLoggedIn && (
+            <li onClick={onLogout}>Cerrar sesión</li>
+          )}
           <a href="https://github.com/GROOTScorer/acortador-url"><img alt="" className='github' width="25px" height="25px" title="Repositorio" src="https://cdn-icons-png.flaticon.com/256/25/25231.png"></img></a>
         </ul>
       </nav>
