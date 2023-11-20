b7f976d65ad93480ed32e1c1981419d6c79ededd
GROOTScorer <gaeldlk@gmail.com>
2023-11-20 16:08:15 -0300

    Agrega estado de Login y Descripción

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Main.css';
 
-function Main() {
-  const [url, setUrl] = useState('');
+function Main(props) {
   const [shortUrl, setShortUrl] = useState('');
+  const [descripcionRecibida, setDescripcionRecibida] = useState('');
 
-  const handleSubmit = async (event) => {
+  async function handleSubmit(event)
+  {
     event.preventDefault();
+    const formData = new FormData(event.target);
+    const url = formData.get('url');
+    const descripcion = formData.get('descripcion');
+
     try {
       const response = await fetch('http://localhost:8000/createshorturl', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ url }),
+        body: JSON.stringify({ url, descripcion })
       });
 
       if (!response.ok) {
@@ -22,37 +28,58 @@ function Main() {
 
       const data = await response.json();
       setShortUrl(data.shortUrl);
+      setDescripcionRecibida(data.descripcion);
+      event.target.reset();
     } catch (error) {
       console.error('Error al acortar la URL:', error);
     }
   };
 
+  function Copiar()
+  {
+    navigator.clipboard.writeText(shortUrl)
+      .then(() => alert("URL copiada al portapapeles: " + shortUrl))
+      .catch(err => console.error('Error al copiar al portapapeles:', err));
+  };
 
   return (
     <>
-    <div className='Home'>
-      <div className="Container">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="link">Link:</label>
-        <input
-          type="text"
-          placeholder="Ingresa la URL aquí"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
-        <br/>
-        <button type="submit">Recortar</button>
-      </form>
+      <div className='Home'>
+        <div className="Container">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="link">Link:</label>
+            <input
+              name="url"
+              type="text"
+              placeholder="Ingresa la URL"
+            />
+            <br/>
+            <label htmlFor="descripcion">Descripción:</label>
+            <input
+              name="descripcion"
+              type="text"
+              placeholder="Ingresa la descripción"
+            />
+            <br/>
+            {props.isLoggedIn ? <button type="submit">Recortar</button>
+            : (
+              <>
+                <Link to='/Login'><button>Recortar</button></Link>
+                <p className='noLog'>No estás logeado. Inicia sesión para recortar.</p>
+              </>
+            )}
+          </form>
 
-      {shortUrl && (
-        <p>
-          URL Acortada: <a href={shortUrl} target="_blank" rel="noopener noreferrer">{shortUrl}</a>
-        </p>
-      )}
+          {shortUrl && (
+            <>
+              <p>URL Acortada: <a href={shortUrl} target="_blank" rel="noopener noreferrer">{shortUrl}</a></p>
+              <button onClick={Copiar}>Copiar URL</button>
+              <p>Descripción: {descripcionRecibida}</p>
+            </>
+          )}
+        </div>
+        <footer>Creado por: Gael De Luca - Joaquín Pocovi - Mateo Villegas - Matías Zaracho</footer>
       </div>
-      <footer>Creado por: Gael De Luca - Joaquín Pocovi - Mateo Villegas - Matías Zaracho</footer>
-    </div>
-
     </>
   );
 }
